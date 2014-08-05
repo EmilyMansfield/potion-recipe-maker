c3cc1163ff6a4701ffc17e48556745da361704c4
Daniel Mansfield <daniel.mansfield99@gmail.com>
2014-08-05 11:47:16 +0100

    Delete regex-test.rb
    Test file, shouldn't have been comitted

--- regex-test.rb
+++
@@ -1,8 +0,0 @@
-$data = "Cyclops: rare [Eyes Teeth]"
-
-tokens = $data.match(/(?<name>\w+):\s*(?<rarity>\w+)\s+(?:(?<blacklist>!)?\[(?<items>.*?)\])?\n?/)
-puts "Name = #{tokens['name']}"
-puts "Rarity = #{tokens['rarity']}"
-puts "Blacklist? #{tokens['blacklist'] ? 'Yes' : 'No'}"
-items = tokens['items'].split
-puts "Items = #{items}"
